d0dbc3e428b9dbe3d44ef18c99ae42d5c0445e5a
trieumb <trieu.nv209170@sis.hust.edu.vn>
2023-01-14 23:34:12 +0700

    test

--- back-end/routers/todos.js
+++ back-end/routers/todos.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { addTodo, getTodo } from "../controllers/todo.js";
 
-import { verifyToken } from "../verifyToken.js";
+import { loginStatus } from "../controllers/userControllers"
 
 const router = express.Router();
 
 // cần verifyToken mới được add Todo : ( cần đăng nhập)
-router.post("/", addTodo);
+router.post("/",loginStatus, addTodo);
 router.get("/", getTodo);
 
 export default router;
