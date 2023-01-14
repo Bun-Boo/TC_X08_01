7e1a303889f0660070dfa5d42217ebd7bb5ea4f5
trieumb <trieu.nv209170@sis.hust.edu.vn>
2023-01-14 23:00:40 +0700

    A

--- back-end/index.js
+++ back-end/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended : false}))
 app.use(bodyParser.json())
-// app.use(errorHandler);
+app.use(errorHandler);
 
 const connect = () =>
   mongoose
