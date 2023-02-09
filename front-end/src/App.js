0fb14acb561b71865a20fe5bd7771540e580f0ba
Gnol551 <baolong551@gmail.com>
2023-02-09 22:49:39 +0700

    a

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,10 @@
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import ListTodo from "./list-todo/ListTodo";
 import TestPage from "./pages/TestPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
@@ -11,12 +13,14 @@ function App() {
       {/* <SignIn /> */}
       {/* <SignUp /> */}
       {/* <Home /> */}
+       {/* <ListTodo/>  */}
       <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="login" element={<SignIn />} />
             <Route path="register" element={<SignUp />} />
+            <Route path="listtodo" element={<ListTodo />} /> 
           </Route>
         </Routes>
       </BrowserRouter>
