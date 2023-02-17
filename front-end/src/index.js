dcb31dc2268d735538592ec597f80b29e29e4b0b
Bun-Boo <ios02052001@gmail.com>
2023-02-17 14:49:07 +0700

    fix direct

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./list-todo/styles/GlobalStyles.css";
+// import "./list-todo/styles/GlobalStyles.css";
+import "./components/styles/GlobalStyles.css";
 import { Provider } from "react-redux";
-import { store } from "./list-todo/App/Sotre";
+// import { store } from "./list-todo/App/Sotre";
+import { store } from "./components/App/Sotre";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-        <Provider store={store}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
